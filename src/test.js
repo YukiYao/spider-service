581e7fb1bc592256f20524bfd7be8f69760915e7
Yao Yundong <yaoyundong@bertadata@com>
2018-03-08 19:00:49 +0800

    .

--- src/test.js
+++ src/test.js
@@ -1,6 +1,8 @@
 const Download = require('./core/download')
 const Promise = require('bluebird')
 const cheerio = require('cheerio');
+const superagent = require('superagent');
+
 // const download = async function (fileUrl) {
 //     let info = await Download.downloadFile(fileUrl)
 //     console.log(info)
@@ -15,3 +17,18 @@ const cheerio = require('cheerio');
 // })
 
 const reptileUrl = "http://www.jianshu.com/";
+superagent.get(reptileUrl).end(function (err, res) {
+    // 抛错拦截
+    if (err) {
+        throw Error(err);
+    }
+    /**
+    * res.text 包含未解析前的响应内容
+    * 我们通过cheerio的load方法解析整个文档，就是html页面所有内容，可以通过console.log($.html());在控制台查看
+    */
+    let $ = cheerio.load(res.text);
+    $('#list-container .note-list li').each(function (i, elem) {
+        // 拿到当前li标签下所有的内容，开始干活了
+        console.log()
+    });
+});
